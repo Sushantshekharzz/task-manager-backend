72a8081ea6e467637470176c652304d03889e027
Sushant <sushantshekhar01011999@gmail.com>
2025-01-20 19:11:37 +0530

    Issue in prod checking

--- sequelize/sequelize.js
+++ sequelize/sequelize.js
@@ -1,5 +1,9 @@
 var Sequelize =  require('sequelize')
 require('dotenv').config();
+console.log('DB:', process.env.DB);
+console.log('HOST:', process.env.HOST);
+console.log('USER:', process.env.USER);
+console.log('PASSWORD:', process.env.PASSWORD)
 
 
 const sequelize  =  new Sequelize(process.env.DB, process.env.USER, process.env.PASSWORD,
